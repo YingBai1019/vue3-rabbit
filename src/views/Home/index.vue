124ceab3524876a7c1a6c6021b11b56c16c219d6
Ying <hanning1019@gmail.com>
2023-08-20 15:15:39 +0100

    homenew

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,14 +1,16 @@
 <script setup>
-
 import HomeBanner from '@/views/Home/component/HomeBanner.vue'
 import HomeCategory from '@/views/Home/component/HomeCategory.vue'
-
+import HomePanel from './component/HomePanel.vue';
+import HomeNew from './component/HomeNew.vue';
 </script>
 
 <template>
   <div >
     <HomeCategory></HomeCategory>
     <HomeBanner> </HomeBanner>
+    <HomeNew></HomeNew>
   </div>
+
 
 </template> 
